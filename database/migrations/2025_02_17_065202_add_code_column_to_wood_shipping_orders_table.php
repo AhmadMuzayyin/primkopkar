98b6217581b994f60396bf63b9b7671fde75438b
AhmadMuzayyin <muzayyin654@gmail.com>
2025-02-17 10:18:38 +0700

    Update POS

--- database/migrations/2025_02_17_065202_add_code_column_to_wood_shipping_orders_table.php
+++ database/migrations/2025_02_17_065202_add_code_column_to_wood_shipping_orders_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('payments', function (Blueprint $table) {
+        Schema::table('wood_shipping_orders', function (Blueprint $table) {
             $table->string('code')->unique()->after('id');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('payments', function (Blueprint $table) {
+        Schema::table('wood_shipping_orders', function (Blueprint $table) {
             $table->dropColumn('code');
         });
     }
